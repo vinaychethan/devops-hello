c56a35417e955945f7f9e6bd5dc960ebeec687f5
Vinay Chethan <vinaychethanreddy@gmail.com>
2026-01-21 19:12:41 +0530

    Demo change

--- index.js
+++ index.js
@@ -2,16 +2,13 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.json({ message: 'Hello DevOps' });
+  res.json({ message: 'Hello, This is Demo' });
 });
 
 const port = process.env.PORT || 3000;
 
-// 🔑 ONLY start server if file is run directly
-if (require.main === module) {
-  app.listen(port, () => {
-    console.log(`App running on port ${port}`);
-  });
-}
+app.listen(port, () => {
+  console.log(`App running on port ${port}`);
+});
 
 module.exports = app;
